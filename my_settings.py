2a34cb90aefabbaee8e955b96c31ecd189734944
Nick <nickszap@gmail.com>
2014-10-29 13:13:49 -0500

    Checking that using the user-input times gives the same results as before.

--- my_settings.py
+++ my_settings.py
@@ -17,8 +17,8 @@ fDirData = '/data02/cases/summer2006/eraI/pv/'
 filesData = sorted(glob.glob(fDirData+'eraI_theta-u-v_2pvu_2006-07-20*.nc'), key=os.path.getmtime)
 deltaT = 6.*60.*60. #timestep (s)
 #select time intervals within filesData[iFile]...end[-1] means use all times
-iTimeStart_fData = [0]
-iTimeEnd_fData = [-1]
+iTimeStart_fData = [48]
+iTimeEnd_fData = [88]
 if (True):
   nFiles = len(filesData)
   if (len(iTimeStart_fData) != nFiles or len(iTimeEnd_fData) != nFiles):
@@ -35,11 +35,11 @@ fCorr = fDirSave+'correspond_debug.pkl'
 fTrack = fDirSave+'tracks_debug.txt'
 fMetrics = fDirSave+'metrics_debug.nc'
 
-doPreProc = False
+doPreProc = True
 doSeg = False
 doMetrics = False
 doCorr = False
-doTracks = True
+doTracks = False
 
 def silentremove(filename):
   #from http://stackoverflow.com/questions/10840533/most-pythonic-way-to-delete-a-file-which-may-not-exist
